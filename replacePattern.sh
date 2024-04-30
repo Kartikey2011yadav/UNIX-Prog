2f7c3acf95577a2b5b5fd1ec04ea640f0c064e71
Kartikey2011yadav <nykay10968@gmail.com>
2024-04-30 11:59:35 +0530

    replace Pattern

---
+++ replacePattern.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Replace a Pattern in a Fille
+
+read -p "Enter filename: " filename
+read -p "Enter a pattern to replace: " pattern
+read -p "Enter new pattern: " new_pattern
+grep -q $pattern $filename
+if [ $? -eq 0 ]; then
+sed -i "s/$pattern/$new_pattern/g" $filename
+echo "Updated Lines: "
+grep -w -n $new_pattern $filename
+else
+echo "Error! Pattern did not match."
+fi
