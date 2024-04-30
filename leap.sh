caf92ae6d12dd433b939973a9e9379123d6ef8e5
Kartikey2011yadav <nykay10968@gmail.com>
2024-04-30 21:36:45 +0530

    leap

---
+++ leap.sh
@@ -0,0 +1,12 @@
+echo -n "Enter year (YYYY): "
+read y
+a=`echo "$y%4" | bc`
+b=`echo "$y%100" | bc`
+c=`echo "$y%400" | bc`
+if [ $a -eq 0 -a $b -ne 0 -o $c -eq 0 ]
+then 
+echo "$y is leap year"
+else
+echo "$y is not a leap year"
+
+fi
