2c37dc088bebffd885cf9c08a5b3c98738230395
Kartikey2011yadav <nykay10968@gmail.com>
2024-04-30 21:36:15 +0530

    fibo

---
+++ fibo.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+echo "Enter number of terms:"
+read n
+i=0
+a=0
+b=1
+c=0
+echo "The Fibonaci series is:"
+echo $a
+echo $b
+while [ $i -ne $n ]
+do
+	c=`echo "$a+$b"|bc`
+	echo $c
+	a=$b
+	b=$c
+	i=`echo "$i+1"|bc`
+done
