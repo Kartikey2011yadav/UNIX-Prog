6b3dc11dc2a0f412d34afd2fe1716fa4a20de7a1
Kartikey2011yadav <nykay10968@gmail.com>
2024-04-30 21:37:03 +0530

    palindrom

---
+++ palindrom.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+echo "Enter String:"
+read str
+rev_str=`echo $str | rev`
+echo "Reversed String : $rev_str"
+if [ $str = $rev_str ]
+then
+	echo "String is Palindrom"
+else
+	echo "Not Palindrom"
+fi
